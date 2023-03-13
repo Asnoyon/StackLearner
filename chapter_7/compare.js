309779905813e94e21c0dcf6319801741b61f2dd
Asnoyon <blackpain1100@gmail.com>
2023-03-13 22:35:37 +0600

    compare

--- chapter_7/compare.js
+++ chapter_7/compare.js
@@ -6,12 +6,12 @@ let obj2 = {
     a: 20,
     b: 330
 }
-// if(obj1.a===obj2.a && obj1.b === obj2.b){
-//     console.log(true)
-// }
-// else{
-//     console.log(false)
-// }
+if(obj1.a===obj2.a && obj1.b === obj2.b){
+    console.log(true)
+}
+else{
+    console.log(false)
+}
 if(JSON.stringify(obj1)===JSON.stringify(obj2)){
     console.log(true)
 }
