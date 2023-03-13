42db6785dd0d3b0718dd7fe8e712390fea751b80
Asnoyon <blackpain1100@gmail.com>
2023-03-14 05:09:29 +0600

    function

---
+++ chapter_8/funExpresion.js
@@ -0,0 +1,6 @@
+let addition = function(a,b){
+    return a+b;
+}
+console.log(addition(10, 15));
+let another = addition;
+console.log(another(7, 8))
---
+++ chapter_8/return.js
@@ -0,0 +1,9 @@
+function profile(name, email){
+    return { 
+        a: name,
+        b: email
+    }
+    
+}
+let p1= profile('noyon', 't@gmail.com')
+console.log(p1)
