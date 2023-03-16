b820ee408377626c1fdb624e6e551c796d37cf62
Asnoyon <blackpain1100@gmail.com>
2023-03-16 08:09:42 +0600

    basic

---
+++ chapter_2/date.js
@@ -0,0 +1,8 @@
+let date = new Date();
+console.log(date);
+console.log(date.toDateString());
+console.log(date.toTimeString())
+console.log(date.toLocaleString())
+console.log(date.getFullYear());
+console.log(date.getMonth());
+console.log(date.getMinutes())
---
+++ chapter_2/if.js
@@ -0,0 +1,16 @@
+// let a = 100;
+// let b = 20;
+// if(a>b){
+//     console.log(a+ " is greater than "+b)
+// }
+// else{
+//     console.log(b+ " is greater than a " +a)
+// }
+
+let n = 6;
+if (n%2 == 0){
+    console.log(n+ " is even number")
+}
+else{
+    console.log(n+ " is odd number")
+}
