d174a99e15d9fec967763da9d1679afc4d3f4d61
Ashadujjaman Noyon <45801264+Asnoyon@users.noreply.github.com>
2023-03-16 08:13:19 +0600

    Delete date.js

--- date.js
+++
@@ -1,8 +0,0 @@
-let date = new Date();
-console.log(date);
-console.log(date.toDateString());
-console.log(date.toTimeString())
-console.log(date.toLocaleString())
-console.log(date.getFullYear());
-console.log(date.getMonth());
-console.log(date.getMinutes())
