5a502a45f5b0febd212c1db7db5dcbaabc0db238
Asnoyon <blackpain1100@gmail.com>
2023-03-18 16:50:56 +0600

    foreach and map

--- chapter_9/foreach.js
+++ chapter_9/foreach.js
@@ -9,11 +9,16 @@ let array = [1, 2, 3, 4, 5, ];
 
 function forEach(array,cb){
     for(let i = 0; i<array.length; i++) {
-        cb(array[i]);
+        cb(array[i],i,array);
     }
 }
 let sum = 0;
-forEach(array,function (value){
+forEach(array,function (value,index,array){
     sum+=value
 })
 console.log(sum)
+
+forEach(array, function(value,index,array){
+    array[index] = value + 5;
+})
+console.log(array)
---
+++ chapter_9/map.js
@@ -0,0 +1,27 @@
+let arr = [1,2,3];
+let sqArr = arr.map(function (value){
+    // return Math.random() * 100;
+    return value*value
+})
+
+console.log(arr);
+// console.log(sqArr)
+
+function myMap(arr,cb){
+    let newArray = [];
+    for(let i = 0; i<arr.length; i++){
+        let temp = cb(arr[i],i,arr)
+        newArray.push(temp);
+    }
+    return newArray
+}
+
+let qb = myMap(arr, function(value){
+    return value * value * value
+})
+let mTen = myMap(arr, function(value){
+    return value * 10
+})
+console.log(arr);
+console.log(qb);
+console.log(mTen);
