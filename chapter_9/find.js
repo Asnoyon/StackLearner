60ce37c6eeae549aa77488f31c36495a0276d534
Asnoyon <blackpain1100@gmail.com>
2023-03-27 11:39:16 +0600

    find and sort

---
+++ chapter_9/find.js
@@ -0,0 +1,14 @@
+// find function
+let arr = [7, 4, 8, 6, 9, 2, 1, 70, 3];
+
+// let result= arr.find(function(value, index){
+//     return value === 9
+// })
+
+// console.log(result)
+
+// find index
+let result = arr.findIndex(function(value){
+    return value ===9
+})
+console.log(result)
---
+++ chapter_9/sort.js
@@ -0,0 +1,46 @@
+let persons = [
+    {
+    firstName :'A',
+    age : 24
+    },
+    {
+    firstName :'B',
+    age : 19
+    },
+
+    {
+    firstName :'C',
+    age : 26
+    },
+    {
+    firstName :'D',
+    age : 21
+    }
+]
+ let arr = [1, 7, 9 , 8, 4 , 1, 3, 20];
+//  arr.sort();
+//  console.log(arr)
+// persons.sort();
+// console.log(persons)
+
+arr.sort(function(a,b){
+    if(a>b){
+        return 1
+    }else if(a<b){
+        return -1
+    }else{
+        return 0;
+    }
+})
+console.log(arr);
+
+persons.sort(function(a, b){
+    if(a.age>b.age){
+        return 1
+    }else if(a.age<b.age){
+        return -1
+    }else{
+        return 0
+    }
+})
+console.log(persons)
