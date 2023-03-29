49e4b9d727bc111fce731a9c40ba9a996ee0ef7a
Asnoyon <blackpain1100@gmail.com>
2023-03-29 21:29:54 +0600

    return func

---
+++ chapter_9/return.js
@@ -0,0 +1,10 @@
+function greet(msg){
+    function greetings (name){
+        return msg + ', ' +name + '!'
+    }
+    return greetings
+}
+let gm = greet('Good Morning')
+// console.log(typeof gm)
+let msg = gm("Noyon")
+console.log(msg)
