82f75398d7bac2ab8e592491b51d9a77e4988803
Asnoyon <blackpain1100@gmail.com>
2023-04-10 11:58:52 +0600

    getID and getClass

--- Dom/script.js
+++ Dom/script.js
@@ -1,10 +1,26 @@
-console.log("Noyon");
-console.dir(typeof(document));
-console.log(document.title)
-console.log(document.URL)
-console.log(document.head)
-document.title='NEW DOM'
-console.log(document.all)
-for(let element of document.all){
-    console.log(document)
+// console.log("Noyon");
+// console.dir(typeof(document));
+// console.log(document.title)
+// console.log(document.URL)
+// console.log(document.head)
+// document.title='NEW DOM'
+// console.log(document.all)
+// for(let element of document.all){
+//     console.log(document)
+// }
+
+//Get Element By Id
+console.log(document.getElementById('new-task'))
+
+let headerElement=(document.getElementById('header'))
+headerElement.textContent='TO-DO-Apps'
+headerElement.innerText='TO-DO-App'
+headerElement.style.color='green'
+headerElement.style.fontSize='50px'
+
+//Get Element By Class
+let item= document.getElementById('items')
+let items=(item.getElementsByClassName('item'));
+for(i=0; i<items.length; i++){
+    items[i].style.color='red'
 }
