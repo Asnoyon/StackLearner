428c93e253f6904dca64eb51db906751b6ce900b
Asnoyon <blackpain1100@gmail.com>
2023-04-10 11:29:18 +0600

    Dom

---
+++ Dom/script.js
@@ -0,0 +1,10 @@
+console.log("Noyon");
+console.dir(typeof(document));
+console.log(document.title)
+console.log(document.URL)
+console.log(document.head)
+document.title='NEW DOM'
+console.log(document.all)
+for(let element of document.all){
+    console.log(document)
+}
