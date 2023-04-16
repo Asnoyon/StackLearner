e65296faf1276b61146f9916cf6d967c5c76e0b1
Asnoyon <blackpain1100@gmail.com>
2023-04-16 12:33:20 +0600

    sort modify

--- chapter_9/sort.js
+++ chapter_9/sort.js
@@ -17,22 +17,24 @@ let persons = [
     age : 21
     }
 ]
- let arr = [1, 7, 9 , 8, 4 , 1, 3, 20];
+ let arr = [1,200, 7, 9 ,-1, 8,-20, 4 ,-4,-8, 1, 3, 20];
 //  arr.sort();
 //  console.log(arr)
 // persons.sort();
 // console.log(persons)
+arr.sort((a,b)=>a-b)
+console.log(arr)
 
-arr.sort(function(a,b){
-    if(a>b){
-        return 1
-    }else if(a<b){
-        return -1
-    }else{
-        return 0;
-    }
-})
-console.log(arr);
+// arr.sort(function(a,b){
+//     if(a>b){
+//         return 1
+//     }else if(a<b){
+//         return -1
+//     }else{
+//         return 0;
+//     }
+// })
+// console.log(arr);
 
 // persons.sort(function(a, b){
 //     if(a.age>b.age){
@@ -44,3 +46,5 @@ console.log(arr);
 //     }
 // })
 // console.log(persons)
+persons.sort((a,b)=>a.age-b.age);
+console.log(persons)
