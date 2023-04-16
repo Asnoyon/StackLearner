0cb4d65f279c2130541e99b25240573f01acd2ac
Asnoyon <blackpain1100@gmail.com>
2023-04-16 13:10:34 +0600

    sort string done

--- chapter_9/sortString.js
+++ chapter_9/sortString.js
@@ -1,9 +1,44 @@
-let string1 = ['Noyon', 'noyon', 'Apple','Bat','Zen'];
+// let string1 = ['Noyon', 'noyon', 'Apple','Bat','Zen'];
 
-string1.sort((a,b)=>{
-    a = a.toLowerCase()
-    b = b.toLowerCase()
-    if(a>b) return 1;
-    if(b>a) return -1;
-    return 0;
+// string1.sort((a,b)=>{
+//     a = a.toLowerCase()
+//     b = b.toLowerCase()
+//     if(a>b) return 1;
+//     if(b>a) return -1;
+//     return 0;
+// })
+
+let persons = [
+    {
+    firstName :'A',
+    age : 24
+    },
+    {
+    firstName :'B',
+    age : 19
+    },
+
+    {
+    firstName :'C',
+    age : 26
+    },
+    {
+    firstName :'D',
+    age : 21
+    }
+]
+persons.sort;
+console.log(persons);
+persons.sort((a,b)=>{
+    if(a.age>b.age){
+        return 1;
+    }
+    else if(b.age>a.age){
+        return -1;
+    }
+    else{
+        return 0;
+    }
+
 })
+console.log(persons)
