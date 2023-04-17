9d965be230004b5bbaf6edf9252ba5e160b8a55a
Asnoyon <blackpain1100@gmail.com>
2023-04-18 00:17:22 +0600

    for each

--- chapter_9/foreach.js
+++ chapter_9/foreach.js
@@ -1,11 +1,11 @@
 let array = [1, 2, 3, 4, 5, ];
 
-// let sum = 0;
-// array.forEach(function(value){
-//     // console.log(value)
-//     sum+=value
-// })
-// console.log(sum)
+let sum = 0;
+array.forEach(function(value){
+    // console.log(value)
+    sum+=value
+})
+console.log(sum)
 
 function forEach(array,cb){
     for(let i = 0; i<array.length; i++) {
