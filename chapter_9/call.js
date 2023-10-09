ea9e56617e6fb3a2467acd2d0f0700ea6d28224f
Asnoyon <blackpain1100@gmail.com>
2023-10-10 02:34:40 +0600

    call and apply

--- chapter_9/call.js
+++ chapter_9/call.js
@@ -1,15 +1,23 @@
-var pokemon = {
-  firstname: 'Pika',
-  lastname: 'Chu ',
-  getPokeName: function() {
-      var fullname = this.firstname + ' ' + this.lastname;
-      return fullname;
-  }
-};
+// var pokemon = {
+//   firstname: 'Pika',
+//   lastname: 'Chu ',
+//   getPokeName: function() {
+//       var fullname = this.firstname + ' ' + this.lastname;
+//       return fullname;
+//   }
+// };
 
-var pokemonName = function(snack, hobby) {
-  console.log(this.getPokeName() + ' loves ' + snack + ' and ' + hobby);
-};
+// var pokemonName = function(snack, hobby) {
+//   console.log(this.getPokeName() + ' loves ' + snack + ' and ' + hobby);
+// };
 
-pokemonName.call(pokemon,'sushi', 'algorithms'); // Pika Chu  loves sushi and algorithms
-pokemonName.apply(pokemon,['sushi', 'algorithms']); // Pika Chu  loves sushi and algorithms
+// pokemonName.call(pokemon,'sushi', 'algorithms'); // Pika Chu  loves sushi and algorithms
+// pokemonName.apply(pokemon,['sushi', 'algorithms']); // Pika Chu  loves sushi and algorithms
+function sayHello(a,b){
+  console.log(this)
+  console.log("Hello " + this.name+a+b)
+}  
+var obj={name:"Ali"}
+sayHello.call(obj,"how ","are you")
+sayHello.call({name:"My"},"name is ","Noyon");
+sayHello.apply({name:"I "}, ["am from ", "bangladesh"])
