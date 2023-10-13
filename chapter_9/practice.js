15b338653d9c1c5cfe209c5fbf817c1406a628d3
Asnoyon <blackpain1100@gmail.com>
2023-10-13 17:44:41 +0600

    prototype

--- chapter_9/practice.js
+++ chapter_9/practice.js
@@ -1,18 +1,32 @@
-let arr = [1, 2, 3, 4, 5, 6, 7, 8];
-let sqArr = arr.map(function(value){
-    return value*value;
-})
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8];
+// let sqArr = arr.map(function(value){
+//     return value*value;
+// })
 
-console.log(arr);
-console.log(sqArr)
-function greet(msg){
-    function greetings (name){
-        return msg + ', ' +name + '!'
+// console.log(arr);
+// console.log(sqArr)
+// function greet(msg){
+//     function greetings (name){
+//         return msg + ', ' +name + '!'
+//     }
+//     return greetings
+// }
+// let gm = greet('Good Morning')
+// // console.log(typeof gm)
+// let msg = gm("Noyon")
+// console.log(msg)
+// console.log(gm('Twinkle Cats'))
+function computeSum(arr){
+    // console.log(arr.length)
+    if(arr.length === 1){
+        
+      return arr[0];
+   
     }
-    return greetings
-}
-let gm = greet('Good Morning')
-// console.log(typeof gm)
-let msg = gm("Noyon")
-console.log(msg)
-console.log(gm('Twinkle Cats'))
+    else{
+        
+      return arr.pop() + computeSum(arr);
+    }
+  }
+  console.log(computeSum([7, 8, 9, 99])); // Returns 123
+  
